479b308cc1a628344bdd31ce05ca7c6f2a0979ea
Fernando B. Emidio <fernando.bemidio@gmail.com>
2018-05-30 01:51:47 -0300

    versão inicial: service discovery, config-server e authentication

---
+++ authentication-server/src/main/java/com/emidio/AuthenticationServerApplication.java
@@ -0,0 +1,14 @@
+package com.emidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServerApplication.class, args);
+	}
+}
